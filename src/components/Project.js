311083e0ef026cc63bdf56a8fc6890417e349644
BustinMeksmefilgud <escarro.sebastian@gmail.com>
2023-07-17 20:13:03 +0800

    added new project: Meals on Wheels

--- src/components/Project.js
+++ src/components/Project.js
@@ -66,6 +66,33 @@ class Project extends Component{
                 </div>
               </div>
               
+               {/* Project 3 */}
+              
+               <div class="group bg-teal-200 hover:bg-teal-300 duration-300 p-4">
+                <a target="_blank" href="https://mealsonwheels.yeems214.xyz/">    
+                  <img src="./assets/img/MealsOnWheels.png" alt="A project of mine" class="object-cover w-full h-80 hover:-translate-y-1 hover:scale-60 hover:p-4 duration-300" />
+                </a> 
+                <div class="px-4 py-3 text-gray-800">
+                  <h2 class="text-4xl md:text-5xl font-bold mb-8 text-center hover:-translate-y-1 hover:scale-110 hover:p-4 duration-300"><a target="_blank" href="https://mealsonwheels.yeems214.xyz/">Meals on Wheels</a></h2>
+                  <p>A website that allows members to order hot meals if they are within 10km from the outsourced kitchen but cold meals if above that. Utilizes a donation page
+                    that uses the PayPal api in order to send donations.
+                  </p>
+                  <div class="container flex flex-wrap sm:hidden group-hover:flex duration-300">
+                      <div class="flex rounded-full items-center bg-teal-100 cursor-default hover:bg-teal-500 duration-300 px-4 py-2 mt-4 mr-2">
+                        <h1 class="w-4 h-4 sm:w-6 sm:h-6 bg-green-700 rounded-full mr-2"></h1>
+                          Java
+                      </div>        
+                      <div class="flex rounded-full items-center bg-teal-100 cursor-default hover:bg-teal-500 duration-300 px-4 py-2 mt-4 mr-2">
+                        <h1 class="w-4 h-4 sm:w-6 sm:h-6 bg-green-400 rounded-full mr-2"></h1>
+                          Spring
+                      </div>     
+                      <div class="flex rounded-full items-center bg-teal-100 cursor-default hover:bg-teal-500 duration-300 px-4 py-2 mt-4 mr-2">
+                        <h1 class="w-4 h-4 sm:w-6 sm:h-6 bg-blue-500 rounded-full mr-2"></h1>
+                          MySQL
+                      </div>
+                    </div>
+                </div>
+              </div>
           
             {/*End of Row 1*/}   
 
